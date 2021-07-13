8e829c38afd82035c7150e4f5b7caefc3175572b
Your Name <asaadmohamed8756@gmail.com>
2021-07-13 08:36:35 +0200

    Update chapitre_01

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -4,30 +4,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Box from "./components/Box";
 import './styles/global.css';
 
-const  myCSS = {
-  tempMin: -2,
-  tempMax: 40,
-  heartMin: 80,
-  heartMax: 180,
-  stepsMin: 0,
-  stepsMax: 50000
-};
+const tempMin =  -2;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <h1 className="text-primary">Hello !</h1>
 
-          <p>Heart : {myCSS.heartMin}</p>
-          <p>Temperature : {myCSS.tempMin}</p>
-          <p>Steps : {myCSS.stepsMin}</p>
+          <p>Heart : {heartMin}</p>
+          <p>Temperature : {tempMin}</p>
+          <p>Steps : {stepsMin}</p>
 
-          <Box text="Water" action=""/>
-          <Box text="Steps" action=""/>
-          <Box text="Heart" action=""/>
-          <Box text="Temperature" action=""/>
+          <Box text="Water"  style={{icon : "local_drink"} + {color : "#3A85FF"} + {value : 1.5} + {unit : "L"}}/>{this.props.text}
+          <Box text="Steps" />
+          <Box text="Heart" />
+          <Box text="Temperature" />
           
         </div>
       </div>
--- chapitre_01/elsy/src/components/Box.js
+++ chapitre_01/elsy/src/components/Box.js
@@ -10,7 +10,7 @@ class Box extends React.Component {
     render() {
         return (
             <div className="box col-sm-3 col-6">
-                <span className="material-icons">{this.props.icon} {this.props.color} {this.props.value} {this.props.unit}</span>
+                <span className="material-icons" style={boxStyle}>{this.props.icon} {this.props.color} {this.props.value} {this.props.unit}</span>
             </div>
         );
     }
